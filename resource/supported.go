656bb75a1ebf0cc5080f17ce618c07890d561fee
Alijohn Ghassemlouei <alijohn.ghassemlouei@sapns2.com>
2020-05-08 19:38:43 -0400

    adjusting output variable

--- resource/supported.go
+++ resource/supported.go
@@ -608,7 +608,7 @@ func (a *AWS) cloudTrails() (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	return output.CloudTrail, nil
+	return output.CloudTrails, nil
 }
 
 func (a *AWS) ebsSnapshots() (interface{}, error) {
